380cff92b1bf214d4a86a822bbd41eca912a887c
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-04-18 18:49:59 -0300

    refactor: simplify saver

--- internal/model/persistence.go
+++ internal/model/persistence.go
@@ -2,9 +2,29 @@ package model
 
 import (
 	"fmt"
+	"os"
 
 	"chatbot/internal/config"
 )
+
+func (m *Model) SaveConversation(userInput, botResponse string) error {
+	f, err := os.OpenFile(config.ConversationsFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	info, _ := f.Stat()
+	if info.Size() > 0 {
+		_, err = f.WriteString("\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = f.WriteString(userInput + "|" + botResponse + "\n")
+	return err
+}
 
 func (m *Model) SaveModel() error {
 	fmt.Println("Saving model to", config.ModelFile)
